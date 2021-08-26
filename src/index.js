2cbf5bb76bb22f4df1b6fe3abccbafd089a0eae1
stepnov <eugene.stepnov@gmail.com>
2021-08-26 14:25:36 +0300

    change project structure add german language

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import './translations';
+
 
 ReactDOM.render(
   <React.StrictMode>
