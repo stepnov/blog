12c150d346f2e949da664b25e318c0aaf5601084
stepnov <eugene.stepnov@gmail.com>
2022-03-23 00:17:03 +0300

    Updates

---
+++ precache-manifest.99b1e3b6bcfffe0e0ecf37a47bd71bf1.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "589d6eed5d07285211dd8e74c1d1d0d7",
+    "url": "/blog/index.html"
+  },
+  {
+    "revision": "6c81ae8d93048c2c146c",
+    "url": "/blog/static/css/2.68a81a89.chunk.css"
+  },
+  {
+    "revision": "582b8bd7d2d264cf9619",
+    "url": "/blog/static/css/main.dcafce46.chunk.css"
+  },
+  {
+    "revision": "6c81ae8d93048c2c146c",
+    "url": "/blog/static/js/2.096c8961.chunk.js"
+  },
+  {
+    "revision": "c1bdf306936ddd82567a345834b1f9d5",
+    "url": "/blog/static/js/2.096c8961.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "582b8bd7d2d264cf9619",
+    "url": "/blog/static/js/main.f2342df0.chunk.js"
+  },
+  {
+    "revision": "c6f02b39f7c06218c0be",
+    "url": "/blog/static/js/runtime-main.bb3b46b0.js"
+  },
+  {
+    "revision": "e6d4ecf07daf44ddc7443aef2bc5e2c7",
+    "url": "/blog/static/media/me.e6d4ecf0.jpg"
+  }
+]);
