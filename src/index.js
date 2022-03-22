dd2ce6fa00ed7edcca1873214837367036aecb63
stepnov <eugene.stepnov@gmail.com>
2022-03-22 23:22:26 +0300

    delete translations

--- src/index.js
+++ src/index.js
@@ -4,8 +4,6 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import './translations';
-
 
 ReactDOM.render(
   <React.StrictMode>
